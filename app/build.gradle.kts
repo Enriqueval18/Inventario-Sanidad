ec328d973d6ae03a2111237dccb0345cf79850e1
Bryan Valencia <bryanvalencia173@gmail.com>
2025-05-08 07:37:23 +0200

    Implementacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 }
 
 dependencies {
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     implementation(libs.appcompat)
     implementation(libs.material)
