8ab4e12a03ceffecc66533b6de5b5c0fd7972084
danielcar04 <danielcarriedo04@gmail.com>
2025-05-28 10:35:26 +0200

    configuración del doble inventario (faltan datos)

--- app/src/main/java/com/nickteck/inventariosanidad/Administrador/Dobleinventario.java
+++ app/src/main/java/com/nickteck/inventariosanidad/Administrador/Dobleinventario.java
@@ -1,25 +1,100 @@
 package com.nickteck.inventariosanidad.Administrador;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.nickteck.inventariosanidad.R;
+import com.nickteck.inventariosanidad.Usuario.MostrarInventario;
+import com.nickteck.inventariosanidad.sampledata.MaterialCallback;
+import com.nickteck.inventariosanidad.sampledata.Utilidades;
+
+
 
 public class Dobleinventario extends Fragment {
+
+    private TableLayout tableRowMaterial001;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_doblein, container, false);
-        TableRow tableRowMaterial01 = view.findViewById(R.id.tableRowMaterial001);
-        tableRowMaterial01.setOnClickListener(v -> {
-            MostarDobleinventario dialog = new MostarDobleinventario();
-            dialog.show(getParentFragmentManager(), "MostarDobleinventario");
-        });
+        tableRowMaterial001 = view.findViewById(R.id.tableRowMaterial001);
+        obtenerDatosInventario();
         return view;
     }
+
+    private void obtenerDatosInventario() {
+        Utilidades.obtenerMateriales(new MaterialCallback() {
+            @Override
+            public void onMaterialObtenido(String nombre, int unidades, String almacen, String armario, String estante, int unidades_min, String descripcion) {
+                agregarFila(nombre, unidades, almacen, armario, estante,unidades_min,descripcion);
+            }
+
+            @Override
+            public void onFailure(boolean error) {
+                Log.e("Inventario", "Error al obtener inventario");
+                Toast.makeText(getContext(), "Error al obtener inventario", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void agregarFila(String nombre, int unidades, String almacen, String armario, String estante, int unidades_min, String descripcion) {
+        TableRow fila = new TableRow(getContext());
+        fila.setPadding(4, 4, 4, 4);
+
+        TextView tvNombre = new TextView(getContext());
+        tvNombre.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
+        tvNombre.setText(nombre);
+        tvNombre.setPadding(8, 8, 8, 8);
+
+        TextView tvUnidades = new TextView(getContext());
+        tvUnidades.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
+        tvUnidades.setText(String.valueOf(unidades));
+        tvUnidades.setPadding(8, 8, 8, 8);
+        tvUnidades.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        TextView tvAlmacen = new TextView(getContext());
+        tvAlmacen.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
+        tvAlmacen.setText(almacen);
+        tvAlmacen.setPadding(8, 8, 8, 8);
+
+        TextView tvArmario = new TextView(getContext());
+        tvArmario.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1));
+        tvArmario.setText(armario);
+        tvArmario.setPadding(8, 8, 8, 8);
+
+        fila.addView(tvNombre);
+        fila.addView(tvUnidades);
+        fila.addView(tvAlmacen);
+        fila.addView(tvArmario);
+
+        fila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MostarDobleinventario dialog = new MostarDobleinventario();
+                Bundle args = new Bundle();
+                args.putString("nombre", nombre);
+                args.putInt("unidades", unidades);
+                args.putString("almacen",almacen);
+                args.putString("armario", armario);
+                args.putString("estante", estante);
+                args.putInt("unidadesm", unidades_min);
+                args.putString("descripcion", descripcion);
+                dialog.setArguments(args);
+                dialog.show(getParentFragmentManager(), "MostarDobleinventario");
+            }
+        });
+
+        tableRowMaterial001.addView(fila);
+    }
+
 }
+
--- app/src/main/java/com/nickteck/inventariosanidad/Administrador/MostarDobleinventario.java
+++ app/src/main/java/com/nickteck/inventariosanidad/Administrador/MostarDobleinventario.java
@@ -1,10 +1,15 @@
 package com.nickteck.inventariosanidad.Administrador;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,21 +17,62 @@ import androidx.fragment.app.DialogFragment;
 
 import com.nickteck.inventariosanidad.R;
 
+
+
+
+
 public class MostarDobleinventario extends DialogFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setStyle(DialogFragment.STYLE_NORMAL, R.style.PantallaCompleta);
     }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.propiedades_inventario_doble, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.propiedades_inventario_doble, container, false);
+
+        //TextView tvNombre = view.findViewById(R.id.tvNombre);
+        TextView tvUnidades = view.findViewById(R.id.tvUnidades1);
+        TextView tvAlmacen = view.findViewById(R.id.tvTipoAlmacen1);
+        TextView tvArmarios = view.findViewById(R.id.tvArmario1);
+        //TextView tvEstante = view.findViewById(R.id.tvEstante);
+        TextView tvUnidadesMinimas = view.findViewById(R.id.tvUnidadesMinimas1);
+        //TextView tvDescripcion = view.findViewById(R.id.tvDescripcion);
+
+        Bundle args = getArguments();
+        if (args != null) {
+            String nombre = args.getString("nombre", "");
+            String descripcion = args.getString("descripcion", "");
+            String almacen = args.getString("almacen", "");
+            String armario = args.getString("armario", "");
+            String balda = args.getString("estante", "");
+            int unidades = args.getInt("unidades", 0);
+            int unidadesMin = args.getInt("unidadesm", 0);
+
+            // Usamos el metodo auxiliar para que esté en negrita
+            //tvNombre.setText(createBoldLabel("Nombre: ", nombre));
+            tvUnidades.setText(createBoldLabel("Unidades: ", String.valueOf(unidades)));
+            tvAlmacen.setText(createBoldLabel("Almacén: ", almacen));
+            tvArmarios.setText(createBoldLabel("Armario: ", armario));
+            //tvEstante.setText(createBoldLabel("Estante: ", balda));
+            tvUnidadesMinimas.setText(createBoldLabel("Unidades mínimas: ", String.valueOf(unidadesMin)));
+            //tvDescripcion.setText(createBoldLabel("Descripción: ", descripcion));
+        }
+
+        return view;
+    }
+
+    private SpannableStringBuilder createBoldLabel(String label, String value) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(label);
+        builder.setSpan(new StyleSpan(Typeface.BOLD),
+                0, label.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(value);
+        return builder;
     }
 
     @Override
@@ -42,4 +88,6 @@ public class MostarDobleinventario extends DialogFragment {
         }
     }
 }
+
+
 
